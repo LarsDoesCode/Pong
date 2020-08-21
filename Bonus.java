9f2dd0debbab7746790b1b14509e053a4a3b2079
Lars <kuehn.lars@icloud.com>
2020-08-21 15:58:39 +0200

    Implemented Class Bounce

---
+++ Bonus.java
@@ -0,0 +1,38 @@
+import java.awt.Rectangle;
+
+public class Bonus {
+    public int counterRight, counterLeft;
+    private final int xKoord;
+    private final int yKoord;
+    private final int size;
+    private final Rectangle rectangle;
+    private boolean visible;
+
+    public Bonus(int pX, int pY, int pSize, boolean pVisible) {
+        xKoord = pX;
+        yKoord = pY;
+        size = pSize;
+        visible = pVisible;
+        rectangle = new Rectangle(xKoord, yKoord, size, size);
+    }
+
+    public int getXKoord() {
+        return xKoord;
+    }
+
+    public int getYKoord() {
+        return yKoord;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+}
